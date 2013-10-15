24c08a232800355997ab519a30cbca4a03a0c5b6
Al Pagan <generamics@gmail.com>
2013-10-15 12:49:00 +0000

    Correct compression script to use all commented js in info file

--- js/compress_all.sh
+++ js/compress_all.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 PLUGINS="$DIR/js/plugins"
 rm -f "$PLUGINS.min.js"
 
-grep "^;scripts\[\]" "$DIR/generamics_openspace.info" | head -n -3 | awk '{ print $3 }' | while read LINE
+grep "^;scripts\[\]" "$DIR/generamics_openspace.info" | awk '{ print $3 }' | while read LINE
 do
   cat $LINE >> $PLUGINS.min.js
 done
